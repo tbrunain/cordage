eb4812ca2061577fb92210e74b6db5a1b308cf7a
tbrunain <tristan@settlemint.com>
2020-05-15 23:31:30 +0200

    fix: Load the ethAddress.properties (used in tests)

--- cross-chain-atomic-swap-cordapp/src/main/kotlin/jp/co/layerx/cordage/crosschainatomicswap/Utils.kt
+++ cross-chain-atomic-swap-cordapp/src/main/kotlin/jp/co/layerx/cordage/crosschainatomicswap/Utils.kt
@@ -25,12 +25,16 @@ private val ethAddress = Properties()
 
 fun Party.ethAddress(): String {
     if (ethAddress.isEmpty) {
+        val url = javaClass.classLoader.getResource("ethAddress.properties")
+        if (url != null) {
+            ethAddress.load(url.openStream())
+        }
         val root = System.getProperty("user.dir")
-        val path = Paths.get("$root/cordapps/config/cross-chain-atomic-swap-cordapp-0.1.conf")
+        val path = Paths.get("$root/cordapps/config/config.conf")
         if (Files.isReadable(path)) {
             ethAddress.load(Files.newInputStream(path))
         }
     }
 
-    return ethAddress.getProperty(this.name.organisation, "0x0")
+    return ethAddress.getProperty(this.name.organisation, ethAddress.getProperty(this.name.toString(), "0x0"))
 }
