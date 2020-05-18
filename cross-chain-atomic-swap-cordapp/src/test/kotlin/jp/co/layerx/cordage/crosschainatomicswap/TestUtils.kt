419f96cc9f4d2b034960c69734b00c18f97e115f
tbrunain <tristan@settlemint.com>
2020-05-18 17:27:47 +0200

    feat: Change the way we load the config.conf file in Tests

--- cross-chain-atomic-swap-cordapp/src/test/kotlin/jp/co/layerx/cordage/crosschainatomicswap/TestUtils.kt
+++ cross-chain-atomic-swap-cordapp/src/test/kotlin/jp/co/layerx/cordage/crosschainatomicswap/TestUtils.kt
@@ -1,10 +1,6 @@
 package jp.co.layerx.cordage.crosschainatomicswap
 
-import net.corda.testing.core.ALICE_NAME
-import net.corda.testing.core.BOB_NAME
-import net.corda.testing.core.CHARLIE_NAME
-import net.corda.testing.core.DUMMY_BANK_A_NAME
-import net.corda.testing.core.TestIdentity
+import net.corda.testing.core.*
 import java.util.*
 
 val ALICE = TestIdentity(ALICE_NAME)
@@ -14,12 +10,10 @@ val DUMMY_BANK_A = TestIdentity(DUMMY_BANK_A_NAME)
 
 fun readConfig(key: String): String {
     val properties = Properties()
-    val root = java.lang.System.getProperty("user.dir")
 
-    val path = java.nio.file.Paths.get("$root/src/test/resources/config.conf")
-    if (java.nio.file.Files.isReadable(path)) {
-        properties.load(java.nio.file.Files.newInputStream(path))
+    val url = object{}.javaClass.classLoader.getResource("config.conf")
+    if (url != null) {
+        properties.load(url.openStream())
     }
-
     return properties.getProperty(key, "0x0")
 }
